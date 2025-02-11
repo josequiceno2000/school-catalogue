ed8b6295c14c9daeaa32e7d5f9561a8c52424cd2
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-11 17:42:11 -0600

    Fix __repr__ method

--- main.py
+++ main.py
@@ -17,3 +17,9 @@ class School:
     # Setter
     def set_num_students(self, students):
         self.num_students = students
+
+    # __repr__() method
+    def __repr__(self):
+        return (f"{self.name} is for gifted {self.level} school students. We boast a student body of {self.num_students}!")
+
+
