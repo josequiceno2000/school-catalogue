4d96054db47302ce9497e982f13520979b72664a
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-11 17:26:22 -0600

    Add getters for name, level, num_students

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ class School:
         self.name = name
         self.level = level
         self.num_students = num_students
+    
+    # Getters
+    def get_name(self):
+        return self.name
+    
+    def get_level(self):
+        return self.level
+    
+    def get_num_students(self):
+        return self.num_students
